fc6e4c8e6a2f65895e50dafb60ac2b1de9e5d58a
Chipmunk-Rex <rexgood1005@gmail.com>
2024-08-28 15:21:09 +0900

    feat : GetComponentWithChild

--- ChipmunkLibrary/Utility/ChipmunkLibrary.cs
+++ ChipmunkLibrary/Utility/ChipmunkLibrary.cs
@@ -27,5 +27,58 @@ namespace Chipmunk.Library
             }
             return (T)array.GetValue(0);
         }
+        public static T GetComponentWithChild<T>(GameObject gameObject) where T : Component
+        {
+            if (gameObject.TryGetComponent<T>(out T compo))
+            {
+                return compo;
+            }
+            else
+            {
+                T component = null;
+                Debug.Log(gameObject.name);
+                foreach (Transform child in gameObject.transform)
+                {
+                    component = GetComponentWithChild<T>(child.gameObject);
+                    if (component != null)
+                    {
+                        return component;
+                    }
+                }
+                Debug.Log("There is no component in the child");
+                return component;
+            }
+        }
+        public static T GetComponentWithChild<T>(Component parent) where T : Component
+        {
+            if (parent.TryGetComponent<T>(out T compo))
+            {
+                return compo;
+            }
+            else
+            {
+                T component = null;
+                Debug.Log(parent.name);
+                foreach (Transform child in parent.transform)
+                {
+                    component = GetComponentWithChild<T>(child.gameObject);
+                    if (component != null)
+                    {
+                        return component;
+                    }
+                }
+                Debug.Log("There is no component in the child");
+                return component;
+            }
+        }
+        public static T GetComponenetWhenNullChild<T(GameObject gameObject, ref T component) where T : Component
+        public static T GetComponenetWhenNullChild<T(Component targetCompo, ref T component) where T : Component
+        {
+            if (component == null)
+            {
+                component = GetComponentWithChild<T>(targetCompo);
+            }
+            return component;
+        }
     }
 }
