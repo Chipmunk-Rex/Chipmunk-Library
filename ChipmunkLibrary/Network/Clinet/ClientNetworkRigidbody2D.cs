7e3a5949b36d2dfd267e0a4c60606a8a978bcb5c
Chipmunk-Rex <rexgood1005@gmail.com>
2024-09-03 23:16:30 +0900

    Reapply "feat : network Lib"
    This reverts commit 6c750e62ed04e4a0a5fa64896a38b5303c379b46.

--- ChipmunkLibrary/Network/Clinet/ClientNetworkRigidbody2D.cs
+++ ChipmunkLibrary/Network/Clinet/ClientNetworkRigidbody2D.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode.Components;
 using UnityEngine;
-
-public class ClientNetworkRigidbody2D : NetworkRigidbody2D
+namespace ChipmunkLibrary.Network
 {
+    public class ClientNetworkRigidbody2D : NetworkRigidbody2D
+    {
+    }
 }
